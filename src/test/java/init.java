32c02c86a70e836cd3c0ce0ac0133d982f6e2666
AChorostian <andrzejchorostian@outlook.com>
2019-04-15 09:35:00 +0200

    Add init project files

---
+++ src/test/java/init.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+class initTest
+{
+    @Test
+    void init()
+    {
+        assertTrue(true);
+    }
+}
