937bd13ce769ca6bb08d19ecb1cc959bd05a21e0
Andrzej Chorostian <andrzejchorostian@outlook.com>
2019-05-06 10:55:57 +0200

    Polish code quality

--- src/main/java/OnlineStore/OrderItem.java
+++ src/main/java/OnlineStore/OrderItem.java
@@ -8,20 +8,21 @@ public class OrderItem implements IDataElement
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private int quantity;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Order order;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Item item;
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    private int quantity;
     public int getQuantity() { return quantity; }
     public void setQuantity(int quantity) { this.quantity = quantity; }
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Order order;
     public Order getOrder() { return order; }
     public void setOrder(Order order) { this.order = order; }
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Item item;
     public Item getItem() { return item; }
     public void setItem(User user) { this.item = item; }
 }
--- src/main/java/OnlineStore/User.java
+++ src/main/java/OnlineStore/User.java
@@ -8,19 +8,19 @@ public class User implements IDataElement
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    private String firstName;
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    private String lastName;
     public String getLastName() { return lastName; }
-    public void setLastName(String lastName)
-    { this.lastName = lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    private String eMail;
     public String getEMail() { return eMail; }
     public void setEMail(String eMail) { this.eMail = eMail; }
 }
