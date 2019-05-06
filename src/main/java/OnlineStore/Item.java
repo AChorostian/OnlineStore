9d04ecc80b765a8cfd1679dc8cebefd5266c3025
Andrzej Chorostian <andrzejchorostian@outlook.com>
2019-05-06 10:55:31 +0200

    Polish code quality

--- src/main/java/OnlineStore/Item.java
+++ src/main/java/OnlineStore/Item.java
@@ -8,14 +8,15 @@ public class Item implements IDataElement
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private Double price;
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    private String name;
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    private Double price;
     public Double getPrice() { return price; }
     public void setPrice(Double price) { this.price = price; }
 }
--- src/main/java/OnlineStore/Order.java
+++ src/main/java/OnlineStore/Order.java
@@ -8,11 +8,12 @@ public class Order implements IDataElement
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private User user;
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private User user;
     public User getUser() { return user; }
     public void setUser(User user) { this.user = user; }
 }
